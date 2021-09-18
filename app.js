191d2fec139c81c80c9f4b56a0d4e73a7d10172a
Amirrk79 <amirmntmntmntmnt@gmail.com>
2021-09-18 19:57:36 +0430

    fix: cors

--- app.js
+++ app.js
@@ -13,6 +13,12 @@ const port = process.env.PORT || 8000
 
 app.use(cors())
 
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PATCH, DELETE, OPTIONS');
+    next();
+});
 
 
 io.on('connection' , (socket) => {
